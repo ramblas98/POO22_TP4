fbc3557acdce1f67f7d01a26353502209f31ecd0
alumno <alumno@PCSS04>
2022-05-16 17:47:54 -0300

    declaracion de metodos

---
+++ TP4_EJE1/src/tp4_eje1/Circulo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4_eje1;
+
+/**
+ *
+ * @author alumno
+ */
+public class Circulo extends Figura {
+   private double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+   public double area(){
+   
+   return((3.14)*this.getRadio()*this.getRadio());
+   }
+   
+}
