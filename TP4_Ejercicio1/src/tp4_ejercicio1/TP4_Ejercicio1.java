5e09bde169a5e39bcfecdf2c973dc188e7fa1686
Matias <Matias@LAPTOP-PVUVC0VL>
2022-05-23 17:40:29 -0300

    Ejercicio 1_tp4 comienzo

---
+++ TP4_Ejercicio1/src/tp4_ejercicio1/TP4_Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4_ejercicio1;
+
+/**
+ *
+ * @author Matias
+ */
+public class TP4_Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
